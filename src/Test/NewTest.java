bdf4bcbc391bbe5a9d09045d995004055369c5a3
“tkmseie” <tkmseie@gmail.com>
2016-09-20 13:54:51 +0530

    fixes

--- src/Test/NewTest.java
+++ src/Test/NewTest.java
@@ -60,9 +60,9 @@ public class NewTest extends ReusableMethods {
 		//createdFirstReportPortion();
 try{
 	//Opens firefox driver
-	System.out.println("Inside script");
+	//System.out.println("Inside script");
 		//System.setProperty("webdriver.gecko.driver", "<Path to your WebDriver>");
-	System.out.println("Inside script");
+	//System.out.println("Inside script");
 
 		driver= new FirefoxDriver();
 		
